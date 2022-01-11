417b1d6e86b59b68db2d5c892ce6b01d29565a80
Kethavath Siva Naik <sivanaikk0903@gmail.com>
2022-01-11 07:23:50 +0000

    React: Components

---
+++ firstreactapp/src/App.js
@@ -0,0 +1,11 @@
+const ProductCatalog=()=>{
+  return(
+    <div>
+    <img src="https://media.istockphoto.com/photos/woman-hand-holding-cellphone-with-empty-screen-on-white-background-picture-id1294325965?b=1&k=20&m=1294325965&s=170667a&w=0&h=rQWe3BR4tCNbhkuiR-zGa0tsFUv0OYd70Y_mZvIpV-w="></img>
+    <a href="">Buy here</a>
+    <h3>Price: 12,000</h3>
+    </div>
+  )
+}
+
+export default ProductCatalog
---
+++ firstreactapp/src/index.js
@@ -0,0 +1,11 @@
+// esc6 - ecmascript 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ProductCatalog from './App'
+ReactDOM.render(
+<>
+<ProductCatalog/>
+<ProductCatalog/>
+</>,
+document.getElementById("root"))
+
